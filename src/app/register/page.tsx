75aa7ce6612509393f27e37b6f1870d87e9404df
Archer <thivan.nms@gmail.com>
2024-04-09 15:00:06 +0700

    Fixed bugs, wrap header inside session provider, allow login by google account

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 export default function RegisterPage() {
     const [email, setEmail] = useState('');
@@ -76,7 +77,8 @@ export default function RegisterPage() {
                 <div className="my-4 text-center text-gray-500">
                     or login with provider
                 </div>
-                <button className="flex gap-4 justify-center">
+                <button onClick={() => signIn('google')}
+                    className="flex gap-4 justify-center">
                     <Image src={'/google.png'} alt={'google icon'} width={24} height={24} />
                     Login with google
                 </button>
